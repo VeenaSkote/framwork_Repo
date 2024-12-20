70bdffef592f86ae83e8b75f935aa71e025243b5
admin <admin@Veena-Manjunath.mshome.net>
2024-12-20 15:10:39 +0530

    First Commit

---
+++ PracticeMaven/src/test/java/pac1/CreateOrgTest.java
@@ -0,0 +1,17 @@
+package pac1;
+
+import org.testng.annotations.Test;
+
+public class CreateOrgTest {
+	
+	@Test
+	public void createOrgTest() {
+		System.out.println("create org name");
+	}
+	
+	@Test
+	public void modifyOrgTest() {
+		System.out.println("modify org name");
+	}
+
+}
